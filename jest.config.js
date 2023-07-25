5c61731e4f8cfc5bb2cafac7ea79915159ffb05f
Pedro Lima <pedropplz77@gmail.com>
2023-07-25 18:45:44 -0300

    adding jest configurations

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: false,
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1',
+  },
+  roots: [
+    "<rootDir>/src",
+    "<rootDir>/tests",
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+
+};
+
+export default config;
