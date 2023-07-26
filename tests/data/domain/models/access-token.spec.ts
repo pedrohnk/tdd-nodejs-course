ea3e93f9afa6639da8d9d8a96b86371e97eb3f57
Pedro Lima <pedropplz77@gmail.com>
2023-07-26 15:46:16 -0300

    ensure access token expires in 30 minutes

--- tests/data/domain/models/access-token.spec.ts
+++ tests/data/domain/models/access-token.spec.ts
@@ -5,4 +5,8 @@ describe('Access Token MOdel', () => {
     const sut = new AccessToken('any_value')
     expect(sut).toEqual({ value: 'any_value' })
   })
+
+  it('should expire in 1800000 milliseconds', () => {
+    expect(AccessToken.expirationInMilliseconds).toBe(1800000)
+  })
 })
