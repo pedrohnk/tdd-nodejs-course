c203ff080cc694ff2f37953880c2d928c3ff758c
Pedro Lima <pedropplz77@gmail.com>
2023-07-26 22:16:54 -0300

    improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -19,6 +19,7 @@ describe('Facebook Api', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
 
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
@@ -54,6 +55,16 @@ describe('Facebook Api', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+
+  it('should return facebook user', async () => {
+    const facebookUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(facebookUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
