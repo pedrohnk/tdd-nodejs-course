2cce083ac1ee175de8a4baf0cf27ff31ea406a4d
Pedro Lima <pedropplz77@gmail.com>
2023-07-26 21:52:04 -0300

    ensure facebook api gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -16,6 +16,8 @@ describe('Facebook Api', () => {
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -27,6 +29,17 @@ describe('Facebook Api', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
